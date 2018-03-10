89d4828680584496c7689f9b731b1620bf247c88
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-03-10 18:03:58 +0700

    fix facades
     On branch master
     Changes to be committed:
    	modified:   src/Http/Controllers/KetimpanganRegionalController.php

--- src/Http/Controllers/KetimpanganRegionalController.php
+++ src/Http/Controllers/KetimpanganRegionalController.php
@@ -5,7 +5,7 @@ namespace Bantenprov\KetimpanganRegional\Http\Controllers;
 /* Require */
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Bantenprov\BudgetAbsorption\Facades\KetimpanganRegionalFacade;
+use Bantenprov\KetimpanganRegional\Facades\KetimpanganRegionalFacade;
 
 /* Models */
 use Bantenprov\KetimpanganRegional\Models\Bantenprov\KetimpanganRegional\KetimpanganRegional;
